8f5a2d4001ffb7c8343b46ab9f6c7e74ecf03ac8
Vladimir Kravchenko <56361236+vlad-kravchenko@users.noreply.github.com>
2020-03-31 11:27:14 +0300

    Initial

---
+++ ColorPicker/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace ColorPicker
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RedSlider_OnValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Rectangle.Fill = new SolidColorBrush(Color.FromRgb((byte) RedSlider.Value, (byte) GreenSlider.Value, (byte) BlueSlider.Value));
+        }
+
+        private void Button_OnClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(Rectangle.Fill.ToString());
+            Clipboard.SetText(Rectangle.Fill.ToString());
+        }
+    }
+}
